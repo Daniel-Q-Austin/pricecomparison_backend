e1fa166fe00802107402fe9fc64575561c02b3d9
Dan Zhou <46627274+dan-ster@users.noreply.github.com>
2020-08-03 16:47:33 -0400

    Added image url and cart url and retailer name

--- bestbuyproducts.py
+++ bestbuyproducts.py
@@ -1,3 +1,13 @@
+"""
+This artificat is implemented as an API using flask.
+It retrieves the first ten product results from BestBuy's website based on a user input keyword. ItemName, price, url, retailer name, imageUrl are retrieved.
+Returns the list of products in json format.
+Programmer: Daniel Zhou
+Variables:
+    search: Keyword to search (string)
+    data: Retrieves name, price, and url of items/products (dictionary)
+    products: List of retrieved products. Returned in json format.
+"""
 from flask import request, jsonify, Blueprint
 import urllib.request, json 
 from Administrator import sendResponse
@@ -14,12 +24,13 @@ def api_all():
 			search = request.args['search']
 		else:
 			response['error_2'] = "Error: No search field provided. Please search for products."
-		link = "https://api.bestbuy.com/v1/products(search={})?format=json&show=sku,name,salePrice&apiKey=N0ReEPP28MPw3Gd2xSIAQ5dM".format(search)
-		data = json.loads(urllib.request.urlopen(link).read().decode())
-		products = data['products']
-		for item in products:
-			del item["sku"]
-		products = sorted(products, key = lambda i: i['salePrice']) 
+        link = "https://api.bestbuy.com/v1/products(search={})?format=json&show=name,salePrice,addToCartUrl,largeImage&apiKey=N0ReEPP28MPw3Gd2xSIAQ5dM".format(search)
+        data = json.loads(urllib.request.urlopen(link).read().decode())
+        products = data['products']
+        for item in products:
+            item['url']  = item.pop('addToCartUrl')
+            item['retailer'] = 'BestBuy'
+        products = sorted(products, key = lambda i: i['salePrice']) 
 		response['result'] = products
 	except Exception as err:
 		response['error'] = str(err)
